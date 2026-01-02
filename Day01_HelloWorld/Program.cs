ca605432cc8516ca6cd010def960840cbbf36e55
Athavan M <athavanmrec@gmail.com>
2026-01-02 15:05:07 +0530

    Day 1: Setup solution, Hello World console app, gitignore

---
+++ Day01_HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿// Day 01 Learning
+
+// Project type Console APP
+
+// 1.Setup 
+// 2.Writing first Program on Learning Path
+
+// Writing "Hellow World " To console
+
+Console.WriteLine("Hello, World!");
+
+// Get input from User and Reply Welcome to user
+
+Console.WriteLine("Enter Your Name:");
+string? inputName = Console.ReadLine();
+Console.WriteLine($"Hello, Welcome {inputName} to .Net Learning Journey");
