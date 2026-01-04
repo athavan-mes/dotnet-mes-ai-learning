64d1c24bd492a11d62ce80525fd0281ad68b4d98
Athavan M <athavanmrec@gmail.com>
2026-01-04 10:38:40 +0530

    Day 4: Arrays Task-01

--- Fundamentals/Day04_Arrays_Lists/Day04_Arrays_Lists/Program.cs
+++ Fundamentals/Day04_Arrays_Lists/Day04_Arrays_Lists/Program.cs
@@ -8,6 +8,8 @@
 
 // 1.Array   - can be fixed no of elements
 
+using System.Reflection.PortableExecutable;
+
 string[] machines = {"CNC-01", "CNC-02", "CNC-03"};
 
 for (int i = 0; i < machines.Length; i++)
@@ -63,14 +65,62 @@ string[] names = { "CNC-01", "CNC-02", "CNC-03", "CNC-04", "CNC-05", "CNC-06", "
 List<string> machineNames = new List<string>();
 machineNames.AddRange(names);
 
-foreach(var  machine in machineNames )
+/*“Can you modify a list inside foreach?”
+
+Answer:
+
+“No.Modifying a collection during foreach throws an exception.
+I either modify before the loop or use a for loop.”
+
+✔ for allows modification
+✔ Index-based control
+
+*/
+
+if (machineNames.Contains("CNC-05"))
+    machineNames.Remove("CNC-05");
+
+foreach (var  machine in machineNames )
 {
-    if (machine == "CNC-05")
-        machineNames.Remove(machine);
-    else if (machine == "CNC-08")
+    
+    if (machine == "CNC-08")
         continue;
     else if (machine == "CNC-09")
         break;
     Console.WriteLine($"Machine Name : {machine}");
 
 }
+
+/*
+Problem
+
+Store hourly production counts for a machine and calculate total production.
+
+Requirements
+
+Create an int[] with 8 values(8 - hour shift)
+
+Calculate total production
+
+Calculate average production   */
+
+int[] hourlyProduction = { 120, 130, 125, 140, 135, 128, 132, 138 };
+
+if(hourlyProduction.Length ==0)
+{
+    Console.WriteLine("No Production Data Available");
+}
+
+int totalProduction = hourlyProduction.Sum();
+Console.WriteLine($"Total Production : {totalProduction}");
+
+// int averageProduction = (int)hourlyProduction.Average(); // can do casting which will truncates the decimal values
+
+double averageProduction = hourlyProduction.Average(); 
+
+Console.WriteLine($"Average Production : {averageProduction:F2}");  // here F2 means Formating to two decimal points
+
+
+
+
+
