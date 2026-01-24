11776ab46c73dea8af7ac2b93dd853ba4e8a1fe7
Athavan M <athavanmrec@gmail.com>
2026-01-24 19:57:01 +0530

    Day 18: Delegates and Events MES design

---
+++ Fundamentals/Day18_Delegates_Events/Day18_Delegates_Events/Models/Machine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day18_Delegates_Events.Models
+{
+    public class Machine
+    {
+        public string Id { get; }
+        public event Action<string, string> StatusChanged;
+        public Machine(string id) 
+        {
+            Id = id;
+        }
+
+        public void  Start()
+        {
+            OnStatusChanged("Running");
+        }
+        public void Stop()
+        {
+            OnStatusChanged("Stopped");
+        }
+
+        protected virtual void OnStatusChanged(string status)
+        {
+            StatusChanged?.Invoke(Id, status);
+        }
+
+    }
+}
---
+++ Fundamentals/Day18_Delegates_Events/Day18_Delegates_Events/Service/MesService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day18_Delegates_Events.Service
+{
+    public class MesService
+    {
+        public void OnMachineStatusChanged(string machineId, string status)
+        {
+            Console.WriteLine($"MES : Machine {machineId} is now {status}");
+        }
+    }
+}
